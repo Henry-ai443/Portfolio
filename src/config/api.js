2a29821779bb0fe3c8466e258b689298fb29c168
Henry Maina <henrymaina2024@outlook.com>
2026-04-16 12:21:24 +0300

    API BASE UPDATE

--- src/config/api.js
+++ src/config/api.js
@@ -1,1 +1,1 @@
-export const API_BASE = import.meta.env.VITE_API_URL || "https://my-portfolio-api-qoov.onrender.comg";
+export const API_BASE = import.meta.env.VITE_API_URL || "https://my-portfolio-api-qoov.onrender.com";
